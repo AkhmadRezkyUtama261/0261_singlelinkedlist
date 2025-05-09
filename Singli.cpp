2074c8efc5220069d60bf87195e5a867d5e54371
AkhmadRezkyUtama261 <ahkmadrezkyutama369@gmail.com>
2025-05-09 08:19:09 +0700

    Menambahkan if dan while

--- Singli.cpp
+++ Singli.cpp
@@ -28,5 +28,31 @@ cin >> nim;
 Node *nodeBaru = new Node;
 nodeBaru->noMhs = nim;
 
+if (START == NULL || nim <= START->noMhs)
+{
+    if ((START !=NULL) && (nim == START->noMhs))
+{
+    cout << "\nDuplikasi noMhs tidak diijinkan\n";
+    return;
+}
+nodeBaru->next = START;
+START = nodeBaru;
+return;
+}
+Node *previous = START;
+Node *current = START;
+
+while ((current != NULL) && (nim >= current->noMhs))
+{
+    if (nim == current->noMhs)
+    {
+        cout <<"\nDuplikasi noMhs Tidak diijinkan\n";
+        return;
+    }
+    previous = current;
+    current = current->next;
+}
+nodeBaru->next = current;
+previous->next = nodeBaru;
     }
 };
