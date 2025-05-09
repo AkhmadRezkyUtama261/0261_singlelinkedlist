ea164724c91340ac25ba03dda9d52c4b73eed731
AkhmadRezkyUtama261 <ahkmadrezkyutama369@gmail.com>
2025-05-09 09:11:23 +0700

    case 5

--- Singli.cpp
+++ Singli.cpp
@@ -164,9 +164,31 @@ int main()
                 cout << endl
                 << "Masukkan no mahasiswa yang dicari : ";
                 cin >> nim;
-
-            }
-        }
-
+if (mhs.Search(nim, &previous, &current) == false)
+cout << endl
+<< "Data tidak ditemukan" << endl;
+else{
+    cout << endl
+    << "Data ditemukan" <<endl;
+    else
+    {
+        cout << endl
+        << "Data ditemukan" << endl;
+        cout << "\nNo Mahasiswa: " << current->noMhs << endl;
+        cout << "\n";
     }
+}
+break;
+case '5':
+{
+}
+break;
+default:
+{cout << "Pilihan salah !." << endl;}
+break;
+ }
+ break;
+ }
+
+    } while (ch != '5');
 }
